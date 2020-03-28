4665ffa50f2014d6a378fabc37bf4f48517df0ab
ankit <ankitsinghwakefit@gmail.com>
2020-03-28 13:56:22 +0530

    basic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+
 
 function createElement(type, props, ...children) {
   return {
@@ -25,16 +24,55 @@ function createTextElement(text) {
   }
 }
 
-const Didact = {
-  createElement,
+function render(element, container) {
+  const dom = element.type == "TEXT_ELEMENT"
+  ? document.createTextNode("")
+  : document.createElement(element.type)
+
+  const isProperty = key => key !== "children"
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(name => {
+      dom[name] = element.props[name]
+    })
+
+  ​  element.props.children.forEach(child =>
+    render(child, dom)
+  )
+  container.appendChild(dom)
 }
 
-const element = Didact.createElement(
-  "div",
-  { id: "foo" },
-  Didact.createElement("a", null, "bar"),
-  Didact.createElement("b")
+let nextUnitOfWork = null
+​
+function workLoop(deadline) {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(
+      nextUnitOfWork
+    )
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
+​
+requestIdleCallback(workLoop)
+​
+function performUnitOfWork(nextUnitOfWork) {
+  // TODO
+}
+
+const Didact = {
+  createElement,
+  render,
+}
+
+/** @jsx Didact.createElement */
+const element = (
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
 )
 
 const container = document.getElementById("root");
-ReactDOM.render(element, container);
+Didact.render(element, container);
